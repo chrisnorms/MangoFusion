e9632b1a75d8d0e7d58b73b2827a781ba95bbc42
Norman <Norman@DESKTOP-BG76A5G>
2026-07-07 14:44:31 +0800

    - Add DbContext and ConnectionString

---
+++ MangoFusionApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangoFusionApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
