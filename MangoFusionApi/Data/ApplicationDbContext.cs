8db2004e0c18644a24c4b97e10a72a0532708700
Norman <Norman@DESKTOP-BG76A5G>
2026-07-07 15:09:56 +0800

    - Add Name in IdentityUser. Use custom ApplicationUser instead of IdentityUser

--- MangoFusionApi/Data/ApplicationDbContext.cs
+++ MangoFusionApi/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using MangoFusionApi.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MangoFusionApi.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
--- MangoFusionApi/Program.cs
+++ MangoFusionApi/Program.cs
@@ -1,4 +1,5 @@
 using MangoFusionApi.Data;
+using MangoFusionApi.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
